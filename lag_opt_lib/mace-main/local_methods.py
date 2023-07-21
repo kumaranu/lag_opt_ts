1150832466f3e93e10c0baa31fb1cf68b40c4b43
kumaranu <anukumar94663@gmail.com>
2023-07-20 18:47:49 -0700

    Working TS code that I modified with Eric Yuan.

--- lag_opt_lib/mace-main/local_methods.py
+++ lag_opt_lib/mace-main/local_methods.py
@@ -45,22 +45,21 @@ def newton_raphson(
         )
         mlcalculator.calculate(atoms)
 
-
         hessian = np.reshape(mlcalculator.results['hessian'], (3 * natoms, 3 * natoms))
         eigenvalues, eigenvectors = np.linalg.eig(hessian)
         print('count, energy, imag_eigval:', count, mlcalculator.results['energy'], np.min(eigenvalues.imag))
 
-        # print('eigenvalues.real:\n', eigenvalues.real)
-        # print('eigenvectors.real:\n', eigenvectors.real)
-        # print('eigenvectors.imag:\n', eigenvectors.imag)
-
-        forces = 1 * np.reshape(mlcalculator.results['forces'], (3 * natoms, 1))
+        #gradient = -np.reshape(mlcalculator.results['forces'], (3 * natoms, 1))
+        gradient = -np.reshape(mlcalculator.results['forces'], (3 * natoms,))
 
         # try:
             # dr = np.matmul(np.linalg.inv(hessian), forces)
         dr = np.zeros((3 * natoms, 1))
         for i in range(3 * natoms - 6):
-            dr += np.reshape(eigenvectors.real[:, i], (3 * natoms, 1)) * forces / eigenvalues.real[i]
+            # dr += np.reshape(eigenvectors.real[:, i], (3 * natoms, 1)) * forces / eigenvalues.real[i]
+            dr += np.reshape(eigenvectors.real[:, i] * (np.dot(np.transpose(eigenvectors.real[:, i]),
+                                                    gradient) / eigenvalues.real[i]), (3 * natoms, 1))
+
         '''
         except:
             # Apply damping to the diagonal elements of the Hessian matrix
@@ -70,7 +69,8 @@ def newton_raphson(
         '''
         r = copy.deepcopy(atoms.arrays['positions'])
 
-        atoms.arrays['positions'] = r - np.reshape(dr * step_damping_factor, (natoms, 3))
+        # atoms.arrays['positions'] = r - np.reshape(dr * step_damping_factor, (natoms, 3))
+        atoms.arrays['positions'] = r - np.reshape(dr, (natoms, 3))
 
         trajectory.append(copy.deepcopy(atoms))
         if np.linalg.norm(dr) < threshold:
@@ -88,3 +88,13 @@ if __name__ == '__main__':
     )
     # print('TS:\n', atoms.arrays['positions'])
     write('/home/kumaranu/Downloads/ts_trajectory.xyz', ts_trajectory)
+    mlcalculator = NewtonNet(
+        model_path=newtonnet_model_path.split(":"),
+        settings_path=newtonnet_config_path.split(":"),
+    )
+    mlcalculator.calculate(ts_atoms)
+    natoms = len(ts_atoms)
+    hessian = np.reshape(mlcalculator.results['hessian'], (3 * natoms, 3 * natoms))
+    eigenvalues, eigenvectors = np.linalg.eig(hessian)
+    print('eigenvalues:', eigenvalues)
+
